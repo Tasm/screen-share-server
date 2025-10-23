ab3fa794594e1eb5f436cadf6ab5c373a102be16
Tasm <Tollens@gmail.com>
2025-10-23 11:20:36 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const WebSocket = require('ws');
 const http = require('http');
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // Create HTTP server
 const server = http.createServer();
